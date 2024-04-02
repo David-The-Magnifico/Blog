650c5ae49f4d55aec57c71ce6957d725ca2d121c
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-02 12:35:31 +0100

    Blog App build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

---
+++ src/main/java/africa/semicolon/Blog/data/Model/Comment.java
@@ -0,0 +1,23 @@
+package africa.semicolon.Blog.data.Model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Comments")
+public class Comment {
+    @Id
+    private String id;
+    @DBRef
+    private User commenter;
+    private String comment;
+
+    @Override
+    public String toString() {
+        return String.format("""    
+                id='%s'
+                comment='%s'""", id, comment);
+    }
+}
