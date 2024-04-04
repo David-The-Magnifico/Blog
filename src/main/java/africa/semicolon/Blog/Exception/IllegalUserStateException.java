6091491d1ec397e6e84c2a006841beae3ba6f1f5
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-04 02:52:42 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

---
+++ src/main/java/africa/semicolon/Blog/Exception/IllegalUserStateException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.Blog.Exception;
+
+public class IllegalUserStateException extends BlogThrowsException {
+    public IllegalUserStateException(String message) {
+        super(message);
+    }
+}
