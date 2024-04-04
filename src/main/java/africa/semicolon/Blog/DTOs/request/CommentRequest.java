32902f3ac890d00b90a84d75010a8425aa1d4f67
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-04 02:48:35 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

---
+++ src/main/java/africa/semicolon/Blog/DTOs/request/CommentRequest.java
@@ -0,0 +1,10 @@
+package africa.semicolon.Blog.DTOs.request;
+
+import lombok.Data;
+
+@Data
+public class CommentRequest {
+    private String commenter;
+    private String comment;
+    private String blogPostId;
+}
