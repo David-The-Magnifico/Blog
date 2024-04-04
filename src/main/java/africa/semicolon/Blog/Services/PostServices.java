9110105961ca6cd738219003d361abfd17d9c759
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-04 02:57:02 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

---
+++ src/main/java/africa/semicolon/Blog/Services/PostServices.java
@@ -0,0 +1,16 @@
+package africa.semicolon.Blog.Services;
+
+import africa.semicolon.Blog.DTOs.request.*;
+import africa.semicolon.Blog.DTOs.response.*;
+import africa.semicolon.Blog.data.Model.User;
+import java.africa.semicolon.Blog.data.Model.Post;
+
+
+public interface PostServices {
+    Post createPostWith(CreatePostRequest createPostRequest);
+    EditPostResponse editPostWith(EditPostRequest editPostRequest, Post authorPost);
+    DeletePostResponse deletePostWith(DeletePostRequest deletePostRequest, Post authorPost);
+    ViewPostResponse addViewWith(ViewPostRequest viewPostRequest, User viewer);
+    CommentResponse addCommentWith(CommentRequest commentRequest, User commenter);
+    ViewsCountResponse getNumberOfViews(ViewsCountRequest viewsCountRequest);
+}
