1446506e9bf3accf2a1d6b7e7ae07b5fe8c97d6c
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-07 21:46:04 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

---
+++ src/Test/java/Controllers/PostControllersTest.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import africa.semicolon.Blog.DTOs.request.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PostControllersTest {
+    private RegisterRequest registerRequest;
+    private LoginRequest loginRequest;
+    private CreatePostRequest createPostRequest;
+    private EditPostRequest editPostRequest;
+    private DeletePostRequest deletePostRequest;
+    private ViewPostRequest viewPostRequest;
+    private CommentRequest commentRequest;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getViewsCount() {
+    }
+}
