777dd2ff69c59194d680f41b0947830e1288c25d
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-15 00:15:52 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

--- src/main/java/africa/semicolon/Blog/Exception/PostNotFoundException.java
+++ src/main/java/africa/semicolon/Blog/Exception/PostNotFoundException.java
@@ -1,6 +1,6 @@
 package africa.semicolon.Blog.Exception;
 
-public class PostNotFoundException extends BlogThrowsException {
+public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(String message) {
         super(message);
     }
