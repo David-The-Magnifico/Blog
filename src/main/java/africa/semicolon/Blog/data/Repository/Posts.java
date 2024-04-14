e1dde276e4a1185149128d4692d2255fa86f6e66
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-15 00:16:09 +0100

    Blog App Build
    Signed-off-by: David-The-Magnifico <www.wealthydavid@gmail.com>

--- src/main/java/africa/semicolon/Blog/data/Repository/Posts.java
+++ src/main/java/africa/semicolon/Blog/data/Repository/Posts.java
@@ -3,4 +3,5 @@ package africa.semicolon.Blog.data.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface Posts extends MongoRepository<Posts, String> {
+
 }
